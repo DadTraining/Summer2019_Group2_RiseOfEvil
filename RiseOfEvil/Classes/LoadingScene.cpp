2216c434f0c50ca13212a793c51989af4190d194
Quoc141119 <cubi12c6@gmail.com>
2019-07-05 10:36:48 +0700

    +add file LoadingScene

---
+++ RiseOfEvil/Classes/LoadingScene.cpp
@@ -0,0 +1,65 @@
+#include "LoadingScene.h"
+#include "ui/CocosGUI.h"
+#include "MainMenuScene.h"
+
+Scene * LoadingScene::createScene()
+{
+	return LoadingScene::create();
+}
+
+bool LoadingScene::init()
+{
+	if (!Scene::init())
+	{
+		return false;
+	}
+
+	auto visibleSize = Director::getInstance()->getVisibleSize();
+	Vec2 origin = Director::getInstance()->getVisibleOrigin();
+
+	//create loadingScene background
+	auto loading_bg = Sprite::create("loading_bg.jpg");
+	loading_bg->setScaleX(visibleSize.width / loading_bg->getContentSize().width);
+	loading_bg->setScaleY(visibleSize.height / loading_bg->getContentSize().height);
+	loading_bg->setPosition(visibleSize.width / 2, visibleSize.height / 2);
+	addChild(loading_bg, 0);
+
+	//create loadingbar background
+	auto loadingBar_bg = Sprite::create("loadingbar_bg.png");
+	loadingBar_bg->setScaleX(3);
+	loadingBar_bg->setScaleY(0.1);
+	loadingBar_bg->setPosition(Vec2(visibleSize.width / 2, 100));
+	addChild(loadingBar_bg);
+
+	//create loadingbar
+	static auto loadingbar = ui::LoadingBar::create("loadingbar.png");
+	loadingbar->setScaleX(3.08);
+	loadingbar->setScaleY(0.1);
+	loadingbar->setPercent(0);
+	loadingbar->setDirection(ui::LoadingBar::Direction::LEFT);
+	loadingbar->setPosition(loadingBar_bg->getPosition());
+	addChild(loadingbar);
+
+	//update Loading Bar
+	auto updateLoadingBar = CallFunc::create([]() {
+		if (loadingbar->getPercent() < 100)
+		{
+			loadingbar->setPercent(loadingbar->getPercent() + 0.4);
+		}
+	});
+
+	auto sequenceRunUpdateLoadingBar =	Sequence::createWithTwoActions(updateLoadingBar, DelayTime::create(0.05f));
+
+	auto repeat = Repeat::create(sequenceRunUpdateLoadingBar, 333);
+	auto sequencedelayupdate = Sequence::create(DelayTime::create(1.5), repeat , nullptr);
+	loadingbar->runAction(sequencedelayupdate);
+
+	//replace MainMenuScene
+	auto gotoNext = CallFunc::create([]() {
+		Director::getInstance()->replaceScene(TransitionFade::create(1, MainMenuScene::create(), Color3B(0, 0, 0)));
+	});
+	auto sq_replaceScene = Sequence::create(DelayTime::create(14), gotoNext, nullptr);
+	runAction(sq_replaceScene);
+
+	return true;
+}
--- RiseOfEvil/Classes/LogoScene.cpp
+++ RiseOfEvil/Classes/LogoScene.cpp
@@ -18,27 +18,31 @@ bool LogoScene::init()
 	auto visibleSize = Director::getInstance()->getVisibleSize();
 	Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
-	auto logo = Sprite::create("logo.png");
-	logo->setAnchorPoint(Vec2(0.5, 0.5));
-	logo->setScaleX(visibleSize.width / logo->getContentSize().width);
-	logo->setScaleY(visibleSize.height / logo->getContentSize().height);
-	logo->setPosition(visibleSize.width / 2, visibleSize.height / 2);
-	addChild(logo);
+	//create background logo
+	auto logo_bg = Sprite::create("logo.png");
+	logo_bg->setAnchorPoint(Vec2(0.5, 0.5));
+	logo_bg->setScaleX(visibleSize.width / logo_bg->getContentSize().width);
+	logo_bg->setScaleY(visibleSize.height / logo_bg->getContentSize().height);
+	logo_bg->setPosition(visibleSize.width / 2, visibleSize.height / 2);
+	addChild(logo_bg);
 
+	//create effect
 	auto fadein1 = FadeIn::create(1);
 	auto fadein2 = FadeIn::create(0.2);
 	auto fadeout = FadeOut::create(0.2);
 	auto sequencefade = Sequence::create(fadeout, fadein2, fadeout->clone(), fadein1, nullptr);
 
-	logo->runAction(sequencefade);
+	logo_bg->runAction(sequencefade);
 
+	//create music background
 	auto music_bg = SimpleAudioEngine::getInstance();
 	music_bg->playBackgroundMusic("logomusic.mp3", false);
 
+	//replace LoadingScene
 	auto gotoNext = CallFunc::create([]() {
-		Director::getInstance()->replaceScene(TransitionFade::create(1.5, LoadingScene::create(), Color3B(0, 888, 888)));
+		Director::getInstance()->replaceScene(TransitionFade::create(1.5, LoadingScene::create(), Color3B(0, 0, 0)));
 	});
-	auto sq_replaceScene = Sequence::create(DelayTime::create(3.5), gotoNext, nullptr);
+	auto sq_replaceScene = Sequence::create(DelayTime::create(4), gotoNext, nullptr);
 	runAction(sq_replaceScene);
 
 	return true;
