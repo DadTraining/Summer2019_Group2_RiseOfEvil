19366388aedfb24f1bdc7b1764bf34c7fe02242f
= <=>
2019-07-06 23:06:30 +0700

    Map01

---
+++ RiseOfEvil/Resources/res/WorldScene1/seasons_trees.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="seasons_trees" tilewidth="64" tileheight="64" tilecount="320" columns="16">
+ <image source="tiles/seasons_trees.png" width="1024" height="1280"/>
+</tileset>
