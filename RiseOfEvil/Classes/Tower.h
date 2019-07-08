d6a783d7451fadc42e645c4b8119711262be0cc4
beo19977991 <31729050+beo19977991@users.noreply.github.com>
2019-07-08 17:01:03 +0700

    - edit InIt, Contructor of Tower

--- RiseOfEvil/Classes/Tower.h
+++ RiseOfEvil/Classes/Tower.h
@@ -1,6 +1,11 @@
 #pragma once
 #include "Objects.h"
 #include "cocos2d.h"
+#define ARROW_TOWER 1
+#define MAGIC_TOWER 2
+#define SLOW_TOWER 3
+#define BOMBARD_TOWER 4
+#define BARRACKS_TOWER 5
 using namespace cocos2d;
 class Tower :public Objects
 {
@@ -11,6 +16,8 @@ private:
 	//vector<Skill *>m_listSkill;
 	//vector<Bullet *>m_listBullet;
 	int m_level;
+	int m_cost;
+	//Skill * special;
 public:
 	Tower(Scene *scene);
 	~Tower();
@@ -22,6 +29,7 @@ public:
 	void SetType(int);
 	void SetAttackSpeed(float);
 	void SetRange(int);
+	void SetCost(int);
 	//void SetListSkill(vector<Skill*>);
 	//void SetListBullet(vector<Bullet*>);
 	void SetLevel(int);
@@ -31,4 +39,5 @@ public:
 	//vector<Skill*> GetListSkill();
 	//vector<Bullet*> GetListBullet();
 	int GetLevel();
+	int GetCost();
 };
