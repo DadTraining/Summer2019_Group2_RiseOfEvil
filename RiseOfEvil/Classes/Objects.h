152f42fc3a5ed6b4f2fe8730906afbab464c1864
beo19977991 <31729050+beo19977991@users.noreply.github.com>
2019-07-08 15:48:52 +0700

    + Add class Objects

---
+++ RiseOfEvil/Classes/Objects.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "cocos2d.h"
+using namespace cocos2d;
+class Objects 
+{
+private:
+	Sprite * m_sprite;
+	int m_hitPoint;
+	int m_minimumAtk;
+	int m_maximumAtk;
+	int m_armor;
+
+public:
+	virtual void Init() = 0;
+	virtual void update(float deltaTime) = 0;
+	virtual void Attack(Objects *target) = 0;
+	void SetSprite(Sprite *);
+	void SetHitPoint(int);
+	void SetMinimumAtk(int);
+	void SetMaximumAtk(int);
+	void SetArmor(int);
+	Sprite *GetSprite();
+	int GetHitPoint();
+	int GetMinimumAtk();
+	int GetMaximumAtk();
+	int GetArmor();
+
+	Objects ();
+	~Objects ();
+};
