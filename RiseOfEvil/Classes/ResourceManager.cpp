f3e53346a1df0d11db4b651b84c7ef1169be8a5c
Anh Tai <nqat2003@gmail.com>
2019-07-08 13:53:56 +0700

    + Add DuplicateSprite function in ResourceManager class

--- RiseOfEvil/Classes/ResourceManager.cpp
+++ RiseOfEvil/Classes/ResourceManager.cpp
@@ -93,7 +93,21 @@ Label * ResourceManager::GetLabelById(int id)
 	return m_labels.at(id);
 }
 
-Sprite * ResourceManager::DuplicateSprite(Sprite *)
+Sprite * ResourceManager::DuplicateSprite(Sprite *sprite)
 {
-	return nullptr;
+	Sprite* pRet = Sprite::createWithTexture(sprite->getTexture());
+	if (sprite->getChildrenCount() > 0)
+	{
+		for (int child = 0; child < sprite->getChildrenCount(); child++)
+		{
+			Sprite* spriteChild = (Sprite*)sprite->getChildren().at(child);
+			Sprite* clone = DuplicateSprite((Sprite*)spriteChild);
+			clone->boundingBox() = spriteChild->boundingBox();
+			clone->setContentSize(spriteChild->getContentSize());
+			clone->setPosition(spriteChild->getPosition());
+			clone->setAnchorPoint(spriteChild->getAnchorPoint());
+			pRet->addChild(clone, spriteChild->getZOrder(), spriteChild->getTag());
+		}
+	}
+	return pRet;
 }
