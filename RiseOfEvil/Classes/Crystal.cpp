7c45ed70da9105d29a40cd5b076fccb90d5fc31e
Anh Tai <nqat2003@gmail.com>
2019-07-22 10:57:15 +0700

    + Add class crystal

---
+++ RiseOfEvil/Classes/Crystal.cpp
@@ -0,0 +1,11 @@
+#include "Crystal.h"
+
+int Crystal::getHitPoint()
+{
+	return m_hitpoint;
+}
+
+void Crystal::setHitPoint(int hitPoint)
+{
+	m_hitpoint = hitPoint;
+}
--- RiseOfEvil/Classes/Crystal.h
+++ RiseOfEvil/Classes/Crystal.h
@@ -1,1 +1,11 @@
 #pragma once
+#include "cocos2d.h"
+class Crystal
+{
+private:
+	int m_hitpoint;
+public:
+
+	int getHitPoint();
+	void setHitPoint(int hitPoint);
+};
