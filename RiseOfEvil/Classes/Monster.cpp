52ba3b2e23dd525be385396c38a5c34c1335e356
Anh Tai <nqat2003@gmail.com>
2019-07-23 13:54:04 +0700

    + Add damage effect when monster hit crystal

--- RiseOfEvil/Classes/Monster.cpp
+++ RiseOfEvil/Classes/Monster.cpp
@@ -321,8 +321,15 @@ void Monster::AttackCrystal(Crystal *crystal, float deltaTime)
 {
 	if (countToAttack >= m_attackSpeed)
 	{
-		crystal->setHitPoint(crystal->getHitPoint() - GetDamage());
-		log("%d",crystal->getHitPoint());
+		auto damage = GetDamage();
+		crystal->setHitPoint(crystal->getHitPoint() - damage);
+		auto label = Label::create("", "fonts/Comic_Book.ttf", 20);
+		label->setString("-" + to_string(damage));
+		label->setPosition(Vec2(crystal->getSprite()->getContentSize().width / 2, crystal->getSprite()->getContentSize().height / 2));
+		label->removeFromParent();
+		label->setColor(Color3B::YELLOW);
+		label->runAction(Spawn::create(MoveBy::create(0.4, Vec2(0, 10)), FadeOut::create(0.4), nullptr));
+		crystal->getSprite()->addChild(label);
 		countToAttack = 0;
 	}
 	else
